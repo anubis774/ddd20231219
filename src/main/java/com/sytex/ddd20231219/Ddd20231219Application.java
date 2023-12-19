b9bc71ccfa14ec6569a8e4880eeafa2c5302eabd
Jenny <jenny.chen@xsolutions.com.tw>
2023-12-19 13:07:14 +0800

    Initial commit

---
+++ src/main/java/com/sytex/ddd20231219/Ddd20231219Application.java
@@ -0,0 +1,13 @@
+package com.sytex.ddd20231219;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ddd20231219Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ddd20231219Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sytex/ddd20231219/Ddd20231219ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sytex.ddd20231219;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ddd20231219ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
